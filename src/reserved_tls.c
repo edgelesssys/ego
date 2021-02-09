3d2ad5fa9c07b56d6900c8e36594de7e7680fdf3
Thomas Tendyck <tt@edgeless.systems>
2021-02-09 17:59:34 +0100

    AB#521 add license

--- src/reserved_tls.c
+++ src/reserved_tls.c
@@ -1,3 +1,9 @@
+// Copyright (c) Edgeless Systems GmbH.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 // Reserve TLS space for payload. See comment about reserved_tls lib in
 // CMakeLists.
 __thread char ert_ego_reserved_tls[1024];
