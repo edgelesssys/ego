10020c77ef8eea29eee7f71e329dee76ee273b06
Thomas Tendyck <tt@edgeless.systems>
2021-02-13 17:49:08 +0100

    add marble test

---
+++ cmd/marble-test/main.go
@@ -0,0 +1,92 @@
+// Copyright (c) Edgeless Systems GmbH.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"io/ioutil"
+	"math/big"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+	"time"
+
+	"github.com/edgelesssys/ego/internal/test"
+	"github.com/edgelesssys/marblerun/coordinator/rpc"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+func main() {
+	var t test.T
+	defer t.Exit()
+	require := require.New(&t)
+
+	//
+	// Run a mock Coordinator.
+	//
+
+	server := grpc.NewServer(grpc.Creds(credentials.NewServerTLSFromCert(generateCertificate())))
+	rpc.RegisterMarbleServer(server, marbleServer{})
+
+	listener, err := net.Listen("tcp", "localhost:")
+	require.NoError(err)
+
+	go func() {
+		if err := server.Serve(listener); err != nil {
+			panic(err)
+		}
+	}()
+
+	//
+	// Run the test marble.
+	//
+
+	uuidDir, err := ioutil.TempDir("", "")
+	require.NoError(err)
+	defer os.RemoveAll(uuidDir)
+
+	cmd := exec.Command("erthost", "ego-enclave:test-marble")
+	cmd.Env = append(os.Environ(),
+		"EDG_EGO_PREMAIN=1",
+		"EDG_MARBLE_COORDINATOR_ADDR="+listener.Addr().String(),
+		"EDG_MARBLE_TYPE=type",
+		"EDG_MARBLE_DNS_NAMES=localhost",
+		"EDG_MARBLE_UUID_FILE="+filepath.Join(uuidDir, "uuid"))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{Pdeathsig: syscall.SIGKILL} // kill if parent dies
+	require.NoError(cmd.Run())
+}
+
+type marbleServer struct{}
+
+func (marbleServer) Activate(context.Context, *rpc.ActivationReq) (*rpc.ActivationResp, error) {
+	return &rpc.ActivationResp{Parameters: &rpc.Parameters{
+		Argv: []string{"arg0", "arg1", "arg2"},
+		Env:  map[string]string{"key1": "val1", "key2": "val2"},
+	}}, nil
+}
+
+func generateCertificate() *tls.Certificate {
+	template := &x509.Certificate{
+		SerialNumber: &big.Int{},
+		Subject:      pkix.Name{CommonName: "localhost"},
+		NotAfter:     time.Now().Add(time.Hour),
+	}
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	cert, _ := x509.CreateCertificate(rand.Reader, template, template, &priv.PublicKey, priv)
+	return &tls.Certificate{Certificate: [][]byte{cert}, PrivateKey: priv}
+}
---
+++ cmd/test-marble/main.go
@@ -0,0 +1,24 @@
+// Copyright (c) Edgeless Systems GmbH.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	"os"
+
+	"github.com/edgelesssys/ego/internal/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func main() {
+	var t test.T
+	defer t.Exit()
+	assert := assert.New(&t)
+
+	assert.Equal([]string{"arg0", "arg1", "arg2"}, os.Args)
+	assert.Equal("val1", os.Getenv("key1"))
+	assert.Equal("val2", os.Getenv("key2"))
+}
