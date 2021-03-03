1d3a0d1f5d1daf11f745468edec053d47f3fcc9b
Nils Hanke <nils.hanke@rub.de>
2021-03-03 10:02:31 +0100

    Add file system mounts to premain

--- internal/premain/core/core.go
+++ internal/premain/core/core.go
@@ -7,14 +7,49 @@
 package core
 
 import (
+	"encoding/json"
 	"os"
+	"syscall"
 
+	"github.com/edgelesssys/ego/internal/config"
 	"github.com/edgelesssys/marblerun/marble/premain"
 )
 
 // PreMain runs before the App's actual main routine and initializes the EGo enclave.
 func PreMain(payload string) error {
-	// TODO
+	if len(payload) > 0 {
+		// Load config from embedded payload
+		var config config.Config
+		if err := json.Unmarshal([]byte(payload), &config); err != nil {
+			return err
+		}
+
+		// Perform mounts
+		for _, mountPoint := range config.Mounts {
+			var err error
+
+			switch mountPoint.Type {
+			case "hostfs":
+				if mountPoint.ReadOnly {
+					err = syscall.Mount(mountPoint.Source, mountPoint.Target, "oe_host_file_system", syscall.MS_RDONLY, "")
+				} else {
+					err = syscall.Mount(mountPoint.Source, mountPoint.Target, "oe_host_file_system", 0, "")
+				}
+
+			case "memfs":
+				if mountPoint.ReadOnly {
+					err = syscall.Mount("/", mountPoint.Target, "edg_memfs", syscall.MS_RDONLY, "")
+				} else {
+					err = syscall.Mount("/", mountPoint.Target, "edg_memfs", 0, "")
+				}
+			}
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	// If program is running as a Marble, continue with Marblerun Premain.
 	if os.Getenv("EDG_EGO_PREMAIN") == "1" {
 		return premain.PreMain()
