4837c619c5e4dc5c477f0cd98d98294f75b98186
Thomas Tendyck <tt@edgeless.systems>
2021-04-06 16:28:30 +0000

    simplify MAA sample client

--- samples/azure_attestation/ra_client/client.go
+++ samples/azure_attestation/ra_client/client.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/base64"
-	"encoding/hex"
 	"encoding/json"
-	"encoding/pem"
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 
-	"github.com/dgrijalva/jwt-go"
+	"gopkg.in/square/go-jose.v2"
+	"gopkg.in/square/go-jose.v2/jwt"
 )
 
 // issAddr is the URL of the attestation provider
 const issAddr = "https://shareduks.uks.attest.azure.net"
+
+// privateClaims are some of the private claims of an Azure Attestation token
+type privateClaims struct {
+	SignerID        string `json:"x-ms-sgx-mrsigner"`
+	ProductID       uint   `json:"x-ms-sgx-product-id"`
+	SecurityVersion uint   `json:"x-ms-sgx-svn"`
+	EnclaveHeldData string `json:"x-ms-sgx-ehd"`
+}
 
 func main() {
 	signer := flag.String("s", "", "signer ID")
@@ -32,7 +37,7 @@ func main() {
 		return
 	}
 
-	// Get server certificate and its token. Skip TLS certificate verification because
+	// Get server token. Skip TLS certificate verification because
 	// the certificate is self-signed and we will verify it using the token instead.
 	serverUrl := "https://" + *serverAddr
 	tlsConfig := &tls.Config{InsecureSkipVerify: true}
@@ -42,49 +47,30 @@ func main() {
 	// Get cerificate for token verification from attestation provider.
 	tlsConfig = &tls.Config{}
 	jwkSetBytes := httpGet(tlsConfig, issAddr+"/certs")
-	var keySet JSONWebKeySet
-	if err := json.Unmarshal(jwkSetBytes, &keySet); err != nil {
-		panic(err)
-	}
+	keySet := mustParseKeySet(jwkSetBytes)
 	fmt.Printf("🆗 Loaded attestation provider's verification keys from %s.\n", issAddr+"/certs")
 
 	// Parse token.
-	parts := strings.Split(string(tokenBytes), ".")
-	for i, part := range parts {
-		p, err := base64.RawURLEncoding.DecodeString(part)
-		if err != nil {
-			panic(err)
-		}
-		parts[i] = string(p)
-	}
-	var header JOSEHeader
-	if err := json.Unmarshal([]byte(parts[0]), &header); err != nil {
-		panic(err)
-	}
-
-	// Find signing key.
-	key, err := keySet.findKid(header.Kid)
+	token, err := jwt.ParseSigned(string(tokenBytes))
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("🆗 Found the attestation provider's key which was used to sign the token.")
 
-	// Convert key to PEM, since the provided key of this instance are not valid JSON Web Keys
-	pubk := jwkToRSAPub(key)
-
-	// Parse token.
-	getKey := func(token *jwt.Token) (interface{}, error) { return pubk, nil }
-	token, err := jwt.Parse(string(tokenBytes), getKey)
-	if err != nil {
+	// Verify token and get claims.
+	var publicClaims jwt.Claims
+	var privateClaims privateClaims
+	if err := token.Claims(&keySet, &publicClaims, &privateClaims); err != nil {
 		panic(err)
 	}
-	claims := token.Claims.(jwt.MapClaims)
+	fmt.Println("✅ Token is valid.")
 
-	// Verfify token.
-	mustVerifyTokenClaims(claims, *signer)
+	// Verify token claims.
+	if err := verifyTokenClaims(publicClaims, privateClaims, *signer); err != nil {
+		panic(err)
+	}
 
-	// Verify certificate.
-	certBytes, err := base64.RawURLEncoding.DecodeString(claims["x-ms-sgx-ehd"].(string))
+	// Get certificate.
+	certBytes, err := base64.RawURLEncoding.DecodeString(privateClaims.EnclaveHeldData)
 	if err != nil {
 		panic(err)
 	}
@@ -100,56 +86,58 @@ func main() {
 	fmt.Println("🔒 Sent secret over attested TLS channel.")
 }
 
-// mustVerfiyTokenClaims verifies a bunch of important token claims.
+// verfiyTokenClaims verifies a bunch of important token claims.
 // For productive envornments, please refere to the MS Azure Attestation Documentation
 // and JWT as well as OpenID Connect specification to determine which parameters need
 // to be verified.
-func mustVerifyTokenClaims(claims jwt.MapClaims, signer string) {
-	if err := claims.Valid(); err != nil {
-		panic(err)
+func verifyTokenClaims(publicClaims jwt.Claims, privateClaims privateClaims, signer string) error {
+	if err := publicClaims.Validate(jwt.Expected{Issuer: issAddr}); err != nil {
+		return fmt.Errorf("issuer of token could not be verified: %v", err)
 	}
-	fmt.Println("✅ Token is valid.")
+	fmt.Printf("✅ Issuer of token is %s.\n", publicClaims.Issuer)
 
-	if !claims.VerifyIssuer(issAddr, true) {
-		panic(errors.New("issuer of token could not be verified"))
-	}
-	fmt.Printf("✅ Issuer of token is %s.\n", claims["iss"])
-
-	if claims["x-ms-sgx-mrsigner"] != signer {
-		panic(errors.New("token does not contain the right signer id"))
+	if privateClaims.SignerID != signer {
+		return errors.New("token does not contain the right signer id")
 	}
 	fmt.Println("✅ SignerID of the toke equals the SignerID you passed to the client.")
 
-	if claims["x-ms-sgx-product-id"] != float64(1234) {
-		panic(errors.New("token contains invalid product id"))
+	if privateClaims.ProductID != 1234 {
+		return errors.New("token contains invalid product id")
 	}
 	fmt.Println("✅ Product ID verified.")
 
-	svn, ok := claims["x-ms-sgx-svn"].(float64)
-	if !ok {
-		panic(errors.New("could not get security version number"))
-	}
-	if svn < float64(2) {
-		panic(errors.New("token contains invalid security version number"))
+	if privateClaims.SecurityVersion < 2 {
+		return errors.New("token contains invalid security version number")
 	}
 	fmt.Println("✅ Security Version Number verified.")
+
+	return nil
 }
 
-// jwkToRSAPub extracts a rsa.PublicKey form a JWK's x5u parameter.
+// mustParseKeySet converts the raw keySetBytes to a JSONWebKeySet.
 // This is needed only because the Shared Attestation Provider does not provide valid JWKs.
-func jwkToRSAPub(key *JSONWebKey) *rsa.PublicKey {
-	pemData := key.getPEM()
-	var decodedCertBytes []byte
-	hex.Decode(decodedCertBytes, []byte(key.Certificates[0]))
-	block, _ := pem.Decode([]byte(pemData))
-	if block == nil {
-		panic(errors.New("failed to decode PEM block containing public key"))
+func mustParseKeySet(keySetBytes []byte) jose.JSONWebKeySet {
+	var rawKeySet struct {
+		Keys []struct {
+			X5c []string
+			Kid string
+		}
 	}
-	certAP, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
+	if err := json.Unmarshal(keySetBytes, &rawKeySet); err != nil {
 		panic(err)
 	}
-	return certAP.PublicKey.(*rsa.PublicKey)
+
+	var keySet jose.JSONWebKeySet
+	for _, key := range rawKeySet.Keys {
+		rawCert, _ := base64.StdEncoding.DecodeString(key.X5c[0])
+		cert, err := x509.ParseCertificate(rawCert)
+		if err != nil {
+			panic(err)
+		}
+		keySet.Keys = append(keySet.Keys, jose.JSONWebKey{KeyID: key.Kid, Key: cert.PublicKey})
+	}
+
+	return keySet
 }
 
 func httpGet(tlsConfig *tls.Config, url string) []byte {
@@ -167,42 +155,4 @@ func httpGet(tlsConfig *tls.Config, url string) []byte {
 		panic(err)
 	}
 	return body
-}
-
-// JSONWebKeySet is a set of JSONWebKey.
-type JSONWebKeySet struct {
-	Keys []JSONWebKey `json:"keys"`
-}
-
-// findKid searches a JSONWebKeySet for a key with a given key id.
-func (set JSONWebKeySet) findKid(id string) (*JSONWebKey, error) {
-	for _, key := range set.Keys {
-		if key.KeyID == id {
-			return &key, nil
-		}
-	}
-	return nil, errors.New("could not find kid")
-}
-
-// JSONWebKey represents a JSON Web Key (JWK).
-type JSONWebKey struct {
-	Certificates []string `json:"x5c"`
-	KeyID        string   `json:"kid"`
-	KeyType      string   `json:"kty"`
-}
-
-// getPEM convertrs a JSONWebKey from DER format to PEM.
-func (key JSONWebKey) getPEM() string {
-	pemStart := "-----BEGIN CERTIFICATE-----"
-	x5c := key.Certificates[0]
-	pemEnd := "-----END CERTIFICATE-----"
-	return fmt.Sprintf("%s\n%s\n%s", pemStart, x5c, pemEnd)
-}
-
-// JOSEHeader represents the JOSE header of a JWT
-type JOSEHeader struct {
-	Alg string `json:"alg"`
-	Jku string `json:"jku"`
-	Kid string `json:"kid"`
-	Typ string `json:"typ"`
 }
