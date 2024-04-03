d9415aaf4a75b796958d09958ca76b58f9688675
Thomas Tendyck <tt@edgeless.systems>
2024-04-03 15:17:46 +0200

    allow building newer go module versions

---
+++ ego/cmd/newer-go-ver-test/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "example.com/testmod"
+
+func main() {
+	testmod.Hello()
+}
---
+++ ego/cmd/newer-go-ver-test/testmod/testmod.go
@@ -0,0 +1,7 @@
+package testmod
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("hello from testmod")
+}
