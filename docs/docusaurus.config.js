9737cf7fc60a3d1f4811f58264cf590a5eaaeaa3
Felix Schuster <fs@edgeless.systems>
2025-10-28 11:31:00 +0100

    Set proper canonical URL

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://ego-docs.netlify.app',
+  url: 'https://docs.edgeless.systems',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ego/',
